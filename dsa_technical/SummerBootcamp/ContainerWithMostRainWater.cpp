23314edf2072b48e87b8fd6f8a31b38a07d6b02b
rajveer <itsrajveer29@gmail.com>
2026-06-01 22:21:36 +0530

    upload

--- dsa_technical/SummerBootcamp/ContainerWithMostRainWater.cpp
+++ dsa_technical/SummerBootcamp/ContainerWithMostRainWater.cpp
@@ -1,3 +1,30 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+using namespace std;
 int main()
+{
+    int n;
+    cout << "enter the size of containers :";
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec[i];
+    }
+    int ans = 0;
+    int l = 0;
+    int r = n - 1;
+    while (l < r)
+    {
+        int width = r - l;
+        int height = min(vec[l], vec[r]);
+        int curr = width * height;
+        ans = max(curr, ans);
+        if (vec[l] < vec[r])
+            l++;
+        else
+            r--;
+    }
+    cout << "maximum water :" << ans;
+    return 0;
+}
