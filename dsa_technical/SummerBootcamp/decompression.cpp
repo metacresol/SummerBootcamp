9afa2ce5030f3a8c10a63959fcd0c0ade34bb18a
rajveer <itsrajveer29@gmail.com>
2026-06-01 22:35:58 +0530

    upload

--- dsa_technical/SummerBootcamp/decompression.cpp
+++ dsa_technical/SummerBootcamp/decompression.cpp
@@ -1,9 +1,46 @@
-#include<iostream>
-#include<stack>
-#include<string>
+#include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
-int main(){
+int main()
+{
     string s;
-    cin>>s;
-    stack 
+    cin >> s;
+    stack<int> numberStack;
+    stack<string> stringStack;
+    string curr = "";
+    int num = 0;
+    for (char ch : s)
+    {
+        if (isdigit(ch))
+        {
+            num = num * 10 + (ch - '0'); // convertin string to int.
+        }
+        else if (ch == '[')
+        {
+            numberStack.push(num);
+            stringStack.push(curr);
+            num = 0;
+            curr = "";
+        }
+        else if (ch == ']')
+        {
+            int repeat = numberStack.top();
+            numberStack.pop();
+            string prev = stringStack.top();
+            stringStack.pop();
+            string temp = "";
+            for (int i = 0; i < repeat; i++)
+            {
+                temp += curr;
+            }
+            curr = prev + temp;
+        }
+        else
+        {
+            curr += ch;
+        }
+    }
+    cout << curr;
+    return 0;
 }
