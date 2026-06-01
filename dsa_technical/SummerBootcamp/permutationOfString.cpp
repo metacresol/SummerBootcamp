dd40d4b960e1abd51a495509ea260a4ec38cbfb6
rajveer <itsrajveer29@gmail.com>
2026-06-01 15:12:44 +0530

    upload

---
+++ dsa_technical/SummerBootcamp/permutationOfString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    sort(str.begin(), str.end());
+    do
+    {
+        cout << str << endl;
+    } while (next_permutation(str.begin(), str.end()));
+    return 0;
+}
