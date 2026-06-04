fd0d4f86c7785c8b30e1d253f99de32c54be855a
rajveer <itsrajveer29@gmail.com>
2026-06-04 12:07:56 +0530

    upload project

--- dsa_technical/SummerBootcamp/menuDrivenstack.cpp
+++ dsa_technical/SummerBootcamp/menuDrivenstack.cpp
@@ -5,14 +5,54 @@ using namespace std;
 int main()
 {
     stack<char> s;
-    char ch,val;
-    cin>>ch;
-    do{
-        switch(ch){
-            case 1:
-                {
-                 cin>>val;   
-                }
+    int ch;
+    char val;
+    do
+    {
+        cout << "enter the choice" << endl;
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+        {
+            cout << "enter character" << endl;
+            cin >> val;
+            cout << "Adding Section:" << val << endl;
+            s.push(val);
+            break;
         }
-    }while(ch!=4);
+        case 2:
+        {
+            if (s.empty())
+                cout << "Stack is empty.Cannot pop." << "\n";
+            else
+            {
+                cout << " Removing Section:" << s.top() << endl;
+                s.pop();
+            }
+            break;
+        }
+
+        case 3:
+        {
+            stack<char> temp = s;
+
+            while (!temp.empty())
+            {
+                cout << temp.top() << " ";
+                temp.pop();
+            }
+            cout << endl;
+            break;
+        }
+        case 4:
+        {
+            cout << "Executing program." << endl;
+            break;
+        }
+        default:
+            cout << "Invalid case." << endl;
+        }
+    } while (ch != 4);
+    return 0;
 }
