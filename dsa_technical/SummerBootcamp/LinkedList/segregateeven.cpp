9b4a32667dd8f67705c343a07521f63e1ad3c521
rajveer <itsrajveer29@gmail.com>
2026-06-18 04:38:39 +0530

    upload project

--- dsa_technical/SummerBootcamp/LinkedList/segregateeven.cpp
+++ dsa_technical/SummerBootcamp/LinkedList/segregateeven.cpp
@@ -1,42 +1,64 @@
-#include<iostream>
-pending
+#include <iostream>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
-    Node(int val){
-    data=val;
-    next=NULL; 
-    }  
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
 };
-class list{
-    Node*head=NULL;
-    Node*tail=NULL;
-    public:
-        void insertFront(int val){
-            Node * newnode=new Node(val);
-            if(head==NULL){
-                head=tail=newnode;
-                return;
-            }
-            else{
-                newnode->next=head;
-                head=newnode;
-            }
-        }
-        void display(){
-            Node*temp=head;
-            while(temp!=NULL){
-                cout<<temp->data<<" ";
-                temp=temp->next;
-            }
-        }
-        void segregate(){
+class list
+{
+    Node *head = NULL;
+    Node *tail = NULL;
 
+public:
+    void insert(int val)
+    {
+        Node *newnode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newnode;
+            return;
         }
-
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
 };
-int main(){
-
+int main()
+{
+    list l1, l2;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x % 2 == 0)
+            l1.insert(x);
+        else
+            l2.insert(x);
+    }
+    cout << "Even linked list: ";
+    l1.display();
+    cout << endl;
+    cout << "Odd linked list: ";
+    l2.display();
+    return 0;
 }
